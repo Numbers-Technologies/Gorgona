26ab42b3939887c36bfcda8c57b90ebd5d26525c
Daniil Ermolaev <73552143+blcklptn@users.noreply.github.com>
2022-06-02 02:57:39 +0500

    Add files via upload

--- GorgonaBackend/GorgonaBackend/views.py
+++ GorgonaBackend/GorgonaBackend/views.py
@@ -1,16 +1,21 @@
-#from django.shortcuts import render
+from django.shortcuts import render
+from django.utils.translation import gettext as _
 import sqlite3
+import pwd
+import os
 
-def version_prev():
-    sqlite_connection = sqlite3.connect("/home/numbers/.config/Gorgona/settings.db")
-    cursor = sqlite_connection.cursor()
-    cursor.execute("select * from test;")
-    record = cursor.fetchall()
-    print(record[0][0])
-    sqlite_connection.commit()
-    cursor.close()
 
-version_prev()
+def get_current_version() -> dict:
+        sqlite_connection = sqlite3.connect(f"/home/{pwd.getpwuid(os.getuid()).pw_name}/.config/Gorgona/settings.db")
+        cursor = sqlite_connection.cursor()
+        cursor.execute("select * from test;")
+        record = cursor.fetchall()
+        cursor.close()
+        print("===>", record)
+        return {
+            'report': _(record[0][0])
+        }
 
-#def home(request):
-    #return render(request, 'index.html')
+def home(request):
+    translations = get_current_version()
+    return render(request, 'index.html',context={'translations': translations})
